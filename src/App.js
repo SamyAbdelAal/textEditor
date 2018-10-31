3b4637e7fcdc40a47fdb2659b5bbf402826727b2
Samy AbdelAal <sam.omran@hotmail.com>
2018-10-31 17:02:28 +0300

    Mini project submission

--- src/App.js
+++ src/App.js
@@ -8,13 +8,40 @@ const styles = {
 };
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: "Hello World",
+      color: "",
+      bold: false,
+      italic: false,
+      underline: false
+    };
+
+    this.changeTextColor = this.changeTextColor.bind(this);
+    this.changeTextStyle = this.changeTextStyle.bind(this);
+  }
+
+  changeTextColor(color) {
+    this.setState({ color: color });
+  }
+
+  changeTextStyle(style) {
+    this.setState({ [style]: !this.state[style] });
+  }
+
   render() {
     let stylings = ["bold", "italic", "underline"];
     let colors = ["yellow", "blue", "red", "black", "purple"];
 
     let stylingBoxes = stylings.map(style => {
       return (
-        <button style={styles[style]} key={style}>
+        <button
+          className={`btn ${this.state[style] && "btn btn-primary"}`}
+          style={styles[style]}
+          key={style}
+          onClick={() => this.changeTextStyle(style)}
+        >
           {style}
         </button>
       );
@@ -25,6 +52,7 @@ class App extends Component {
         <button
           style={{ backgroundColor: color, height: 30, width: 30 }}
           key={color}
+          onClick={() => this.changeTextColor(color)}
         />
       );
     });
@@ -35,7 +63,14 @@ class App extends Component {
         {stylingBoxes}
         <br />
         <br />
-        <textarea />
+        <textarea
+          style={{
+            color: this.state.color,
+            fontWeight: this.state.bold ? "bold" : "",
+            fontStyle: this.state.italic ? "italic" : "",
+            textDecorationLine: this.state.underline ? "underline" : ""
+          }}
+        />
         <br />
         {colorBoxes}
       </div>
